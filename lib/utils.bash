f564af02022e730757448692f78c168955a2ae63
Fred Smith <fsmith@wanderu.com>
2025-06-19 17:21:35 -0400

    update docs, clean up TODOs

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for bkl.
 GH_REPO="https://github.com/gopatchy/bkl"
 TOOL_NAME="bkl"
 TOOL_TEST="bkl --version"
